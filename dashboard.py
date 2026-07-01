ef1bbb06d700dc3b697b2d9e74dc220e2b053b31
pgg-18 <PranayGauravGupta@gmail.com>
2026-07-01 11:29:20 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,10 +2,12 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import sqlite3
+from pipeline import check_and_update
 
 st.set_page_config(layout="wide")
 
 if 'updated' not in st.session_state:
+    check_and_update()
     st.session_state['updated'] = True
 
 st.markdown("""
